5dc4ea4ab636b75b2741468b53aeac085ea195ec
Rico <d-Rickyy-b@users.noreply.github.com>
2020-03-21 17:32:46 +0100

    feat: implement conversationhandler
    A conversation handler for the initial contact with the bot

--- Bot.py
+++ Bot.py
@@ -14,6 +14,7 @@ Token = ''
 bot = telegram.Bot(token=Token)
 updater = Updater(token=Token, use_context=True)
 dispatcher = updater.dispatcher
+FEEL_OK, COUGH_FEVER, STRESSED_ANXIOUS, WANNA_HELP, TELL_FRIENDS = range(5)
 
 
 def start(update, context):
@@ -52,12 +53,91 @@ def echo(update, context):
                                                                     "your situation?")
 
 
+yes_no_keyboard = ReplyKeyboardMarkup([["Yes", "No"]])
+yesfilter = Filters.regex('^Yes$')
+nofilter = Filters.regex('^No$')
+
+doctor_room = -403662743
+psychologist_room = -428911749
+newmembers_room = -403519635
+
+
+def cancel(update, context):
+    update.message.reply_text('Bye! I hope we can talk again some day.', reply_markup=ReplyKeyboardRemove())
+    return ConversationHandler.END
+
+
+def areyouok(update, context):
+    update.message.reply_text("Hi! Are you feeling Ok?", reply_markup=yes_no_keyboard)
+    return FEEL_OK
+
+
+def cough(update, context):
+    update.message.reply_text("Oh no, I'm sorry about that! Are you having cough or fever?", reply_markup=yes_no_keyboard)
+    return COUGH_FEVER
+
+
+def stressed(update, context):
+    update.message.reply_text("Good! Are you feeling stressed or anxious?", reply_markup=yes_no_keyboard)
+    return STRESSED_ANXIOUS
+
+
+def wannahelp(update, context):
+    update.message.reply_text("That's great! Do you wanna help?", reply_markup=yes_no_keyboard)
+    return WANNA_HELP
+
+
+def bye(update, context):
+    update.message.reply_text("Okay, Bye!", reply_markup=ReplyKeyboardRemove())
+    return ConversationHandler.END
+
+
+def doctorsroom(update, context):
+    user = update.effective_user
+    context.bot.send_message(chat_id=config.doctor_room, text="A user requested medical help: {}".format(user.first_name))
+    update.message.reply_text("Forwarded your request to the doctor's room!", reply_markup=ReplyKeyboardRemove())
+    return ConversationHandler.END
+
+
+def psychologistsroom(update, context):
+    user = update.effective_user
+    context.bot.send_message(chat_id=config.psychologist_room, text="A user requested psychological help: {}".format(user.first_name))
+    update.message.reply_text("Forwarded your request to the psychologists' room!", reply_markup=ReplyKeyboardRemove())
+    return ConversationHandler.END
+
+
+def newmembersroom(update, context):
+    user = update.effective_user
+    context.bot.send_message(chat_id=config.newmembers_room, text="A user wants to help: {}".format(user.first_name))
+    update.message.reply_text("Forwarded your request to the new members' room!", reply_markup=ReplyKeyboardRemove())
+    return ConversationHandler.END
+
+
+conv_handler = ConversationHandler(
+    entry_points=[CommandHandler('start', areyouok)],
+    states={
+        FEEL_OK: [MessageHandler(yesfilter, wannahelp),
+                  MessageHandler(nofilter, cough)],
+
+        WANNA_HELP: [MessageHandler(yesfilter, newmembersroom),
+                     MessageHandler(nofilter, bye)],
+
+        COUGH_FEVER: [MessageHandler(yesfilter, doctorsroom),
+                      MessageHandler(nofilter, stressed)],
+
+        STRESSED_ANXIOUS: [MessageHandler(yesfilter, psychologistsroom),
+                           MessageHandler(nofilter, bye)]
+    },
+
+    fallbacks=[CommandHandler('cancel', cancel)]
+)
+
 start_handler = CommandHandler('start', start)
 forward_to_room_handler = MessageHandler(Filters.text, forward_to_room)
 response_handler = MessageHandler(Filters.text, response)
 echo_handler = MessageHandler(Filters.text, echo)
 
-
+dispatcher.add_handler(conv_handler)
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(forward_to_room_handler)
 dispatcher.add_handler(response_handler)
