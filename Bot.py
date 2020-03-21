66257550dea22e6273a585a1e53f30f275dcf00e
Rico <d-Rickyy-b@users.noreply.github.com>
2020-03-21 19:03:21 +0100

    fix: remove unused code

--- Bot.py
+++ Bot.py
@@ -16,42 +16,6 @@ dispatcher = updater.dispatcher
 FEEL_OK, COUGH_FEVER, STRESSED_ANXIOUS, WANNA_HELP, TELL_FRIENDS = range(5)
 
 
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="First: add your telegram "
-                                                                    "account. Don't forget the @ !"
-
-                                                                    "Please describe your "
-                                                                    "current situation briefly. "
-                                                                    "Don't forget to mention "
-                                                                    "things like your body "
-                                                                    "temperature.")
-
-
-def forward_to_room(update, context):
-    sleep(15)
-    context.bot.send_message(chat_id='@humanbios0k', text=update.message.text)
-
-
-def response(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="You're information is "
-                                                                    "being processed. A care "
-                                                                    "taker will send you a "
-                                                                    "message with further "
-                                                                    "instructions immediately. "
-                                                                    "Please be patient and "
-                                                                    "sorry for letting you wait")
-
-    # keep patient buisy and calm them down, for more instructions
-    # Keep asking questions, try calming the patient down, while gathering more information for
-    # more information see https://humanbios.org/more
-
-
-def echo(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Okay that helps, thank you "
-                                                                    "can you tell me more about "
-                                                                    "your situation?")
-
-
 yes_no_keyboard = ReplyKeyboardMarkup([["Yes", "No"]])
 yesfilter = Filters.regex('^Yes$')
 nofilter = Filters.regex('^No$')
@@ -127,16 +91,8 @@ conv_handler = ConversationHandler(
     fallbacks=[CommandHandler('cancel', cancel)]
 )
 
-start_handler = CommandHandler('start', start)
-forward_to_room_handler = MessageHandler(Filters.text, forward_to_room)
-response_handler = MessageHandler(Filters.text, response)
-echo_handler = MessageHandler(Filters.text, echo)
 
 dispatcher.add_handler(conv_handler)
-dispatcher.add_handler(start_handler)
-dispatcher.add_handler(forward_to_room_handler)
-dispatcher.add_handler(response_handler)
-dispatcher.add_handler(echo_handler)
 
 updater.start_polling()
 updater.idle()
